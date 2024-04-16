bccac5ee5ccc4c8226ee1ce41f31497d7995c53e
Deepika242 <165794787+Deepika242@users.noreply.github.com>
2024-04-16 20:34:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,64 @@
+
+for(let i=0;i<10;++i)         //task-1
+{
+    console.log("Divya");
+}
+
+for(let i=0;i<10;++i){            //task-2
+    if(i%2==0){
+        even = `${i} is a even number`
+        console.log(even);
+    }
+    else{
+        odd = `${i} is a odd number`
+        console.log(odd);
+    }
+}
+
+
+console.log("Table 17");  //task-3
+let t1=17;
+for(let i=0;i<=10;i++){
+    let va = `${t1} *${i}=${t1*i}`;
+    console.log(va);
+  }
+  console.log("Table 15");
+  let t2=15;
+  for(let i=0;i<=10;i++){
+    let va =`${t2}*${i}=${t1*i}`;
+    console.log(va);
+  }
+  console.log("Table 49");
+  let t3=49;
+  for(let i=0;i<=10;i++){
+    let va =`${t3}*${i}=${t1*i}`;
+    console.log(va);
+  }
+  console.log("Table 99");
+  let t4=99;
+  for(let i=0;i<=10;i++){
+    let va =`${t4}*${i}=${t1*i}`;
+    console.log(va);
+  }
+  console.log("Table 155");
+  let t5=155;
+  for(let i=0;i<=10;i++){
+    let val =`${t5}*${i}=${t1*i}`;
+    console.log(val);
+  }
+
+
+console.log("while loop");   //task-4
+i=0;
+while(i<11){
+    if(i%2==0){
+        even = `${i} is an even number`
+        console.log(even);
+    
+    }
+    else{
+            odd = `${i} is a odd number`
+            console.log(odd);
+    }
+i++;
+}
